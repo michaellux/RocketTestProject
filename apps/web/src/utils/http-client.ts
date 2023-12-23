95df747a1c27c49d576f85929d95963ea899f833
Sidachenko Michael <michaellux@yandex.ua>
2023-12-23 18:52:50 +0300

    change url for deploy

--- apps/web/src/utils/http-client.ts
+++ apps/web/src/utils/http-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:7000",
+  baseURL: import.meta.env.VITE_API_URL ?? "https://combative-stockings-clam.cyclic.app",
 });
 
 export default service;
