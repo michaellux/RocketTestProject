5eb00b3da93d5b2e0585d2d6eac43ea4def45226
Sidachenko Michael <michaellux@yandex.ua>
2023-12-23 19:19:34 +0300

    explicitly allow request from vercel

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://rocket-test-project-web.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
   await app.listen(7000);
 }
 bootstrap();
